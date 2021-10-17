803072a32fc345ab348a332c7712cadd79ff42bd
Abhishek Thory <abhithory@gmail.com>
2021-10-17 14:53:55 +0530

    Updates

---
+++ precache-manifest.763f9adb3bf67b8525095c6756581844.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "7248d3fb07177294557f",
+    "url": "/Our_Storage_Dapp/static/css/main.07697735.chunk.css"
+  },
+  {
+    "revision": "7248d3fb07177294557f",
+    "url": "/Our_Storage_Dapp/static/js/main.7248d3fb.chunk.js"
+  },
+  {
+    "revision": "49b9ce5e99868d83393c",
+    "url": "/Our_Storage_Dapp/static/css/1.66eb0a6f.chunk.css"
+  },
+  {
+    "revision": "49b9ce5e99868d83393c",
+    "url": "/Our_Storage_Dapp/static/js/1.49b9ce5e.chunk.js"
+  },
+  {
+    "revision": "cad83c7928aa0880adc1",
+    "url": "/Our_Storage_Dapp/static/js/runtime~main.cad83c79.js"
+  },
+  {
+    "revision": "e07b538aa51b6fa77f32828af21cb591",
+    "url": "/Our_Storage_Dapp/static/media/bootstrap-icons.e07b538a.woff2"
+  },
+  {
+    "revision": "7fb23f3b56a834d9dcf615b5e7c78da8",
+    "url": "/Our_Storage_Dapp/static/media/bootstrap-icons.7fb23f3b.woff"
+  },
+  {
+    "revision": "ec137282e7efc4f0db5ba52d02e1316c",
+    "url": "/Our_Storage_Dapp/index.html"
+  }
+];
